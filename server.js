be2c55b341bd0d083489cebc0bbdf682f6c20455
Harsha <harsha.sam23@gmail.com>
2021-01-21 23:16:40 +0530

    Added bcrypt, connected to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 const express = require('express');
+// mongoose required to connect to mongodb
+const mongoose = require('mongoose');
+// bcrypt for hashing passwords
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 const body_parser = require('body-parser')
+
+mongoose.connect('mongodb://localhost/quiz', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log("connected")
+})
+.catch((err) => {
+    console.log("error", err)
+})
+
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    pass: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model("User", userSchema);
 
 const app =  express();
 
@@ -32,5 +61,15 @@ app.post('/quiz', (req, res) => {
         questionnaire.push(question);
     });
     res.json("Success");
+})
+app.post('/register', async(req, res) => {
+    const {id, email, name, password} = req.body;
+    const salt = await bcrypt.genSalt(12);
+    const hash = await bcrypt.hash(password, salt);
+    const usr = new User({_id: id, email: email, pass: hash});
+    console.log(usr)
+    usr.save().then(() => console.log("saved")).catch((err) => console.log("error",err))
+    console.log(hash);
+    res.json("register!")
 })
 app.listen(3000)
